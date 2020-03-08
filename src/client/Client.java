d06ab8d4198346ecbeed81c60b7068512550644c
Bruno Marini <bruno.marini1@gmail.com>
2020-03-08 20:34:00 -0300

    [+] Client connect

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,10 +1,26 @@
 package client;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import server.remoteInterfaces.ElectionRemoteInterface;
+import server.remoteObjects.ElectionRemoteObject;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			Registry registry = LocateRegistry.getRegistry("localhost");	
+			ElectionRemoteInterface server = (ElectionRemoteInterface) registry.lookup("Election");
+			System.out.println("[log] Servidor conectado");
+			
+			//Election stub = (Election) registry.lookup("Election");
+		} catch (RemoteException e) {
+			System.err.println("[ERROR] Client (RemoteException): " + e.getMessage());
+		} catch (Exception e) {
+			System.err.println("[ERROR] Client (Exception): " + e.getMessage());
+		}
 	}
 
 }
