9828ac1c399f9d4bfd6097d4684fd9d78fd3e2f3
Bruno Marini <bruno.marini1@gmail.com>
2020-03-08 20:03:04 -0300

    [+] Server listen

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,31 @@
 package server;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import server.remoteInterfaces.ElectionRemoteInterface;
+import server.remoteObjects.ElectionRemoteObject;
+
 public class Server {
-
+	private static final int PORT = 4242;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {			
+			// Create RMI registry
+	        LocateRegistry.createRegistry(PORT); 
 
+	        ElectionRemoteInterface stub = new ElectionRemoteObject();
+	        Registry registry = LocateRegistry.getRegistry();
+	        
+	        // Bind RMI registry with stub
+	        registry.rebind("Election", stub);
+	
+	        System.out.println("[log] Server ready at PORT " + PORT);        
+        } 
+		catch (Exception e) {
+            System.err.println("[ERROR] Server: " + e.getMessage());
+            e.printStackTrace();
+        }
 	}
 
 }
