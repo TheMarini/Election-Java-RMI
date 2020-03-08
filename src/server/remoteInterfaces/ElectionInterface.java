43f5331422bc32c33ad0fdb86519f30b2d2ad766
Bruno Marini <bruno.marini1@gmail.com>
2020-03-08 17:18:26 -0300

    [+] ElectionInterface methods {candidates, vote, result}

--- src/server/remoteInterfaces/ElectionInterface.java
+++ src/server/remoteInterfaces/ElectionInterface.java
@@ -1,5 +1,13 @@
 package server.remoteInterfaces;
 
-public interface ElectionInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
 
+public interface ElectionInterface extends Remote {
+	String[] candidates() throws RemoteException;
+	
+	boolean vote(String nameCandidate, int hashVoter) throws RemoteException;
+	
+	List<List<String>> result() throws RemoteException;
 }
