6d0db1e58c8b8fe0f86b53f87851203909ee7b03
Bruno Marini <bruno.marini1@gmail.com>
2020-03-08 22:15:35 -0300

    [+] Client {connect, candidate}

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,6 @@
 package client;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -8,19 +9,40 @@ import server.remoteInterfaces.ElectionRemoteInterface;
 import server.remoteObjects.ElectionRemoteObject;
 
 public class Client {
-
+	private static Registry registry;
+	private static ElectionRemoteInterface server;
+	
 	public static void main(String[] args) {
-		try {
-			Registry registry = LocateRegistry.getRegistry("localhost");	
-			ElectionRemoteInterface server = (ElectionRemoteInterface) registry.lookup("Election");
-			System.out.println("[log] Servidor conectado");
-			
-			//Election stub = (Election) registry.lookup("Election");
-		} catch (RemoteException e) {
-			System.err.println("[ERROR] Client (RemoteException): " + e.getMessage());
-		} catch (Exception e) {
-			System.err.println("[ERROR] Client (Exception): " + e.getMessage());
+		if (connect()) {
+			System.out.println(candidate());
 		}
 	}
+	
+	public static String[] candidate() {
+		try {
+			return server.candidates();
+		} catch (RemoteException e) {
+			System.err.println("[ERROR] Client on getting candidates: " + e.getMessage());
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
 
+	public static boolean connect() {
+		try {
+			registry = LocateRegistry.getRegistry("localhost");	
+			server = (ElectionRemoteInterface) registry.lookup("Election");
+			
+			// Success
+			System.out.println("[log] Servidor conectado");
+			return true;
+		} catch (RemoteException | NotBoundException e) {
+			System.err.println("[ERROR] Client on connect: " + e.getMessage());
+		}
+		
+		// Error
+		System.err.println("[ERROR] Não foi possível conectar no servidor :(");
+		return false;
+	}
 }
