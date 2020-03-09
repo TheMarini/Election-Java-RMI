f2e1a95185440672749157c34b162c93fdf000d0
Bruno Marini <bruno.marini1@gmail.com>
2020-03-08 23:17:00 -0300

    [+] Client methods {menu, candidate}

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,32 +1,82 @@
 package client;
 
+import java.io.IOException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
 import server.remoteInterfaces.ElectionRemoteInterface;
-import server.remoteObjects.ElectionRemoteObject;
 
 public class Client {
 	private static Registry registry;
 	private static ElectionRemoteInterface server;
 	
 	public static void main(String[] args) {
-		if (connect()) {
-			System.out.println(candidate());
+		if (connect())
+			menu();
+	}
+	
+	public static void menu() {
+		int option = 0;
+		Scanner input = new Scanner(System.in);
+		
+		do {
+			// Show
+			System.out.println("\n           ### Election ###           ");
+			System.out.println("========================================");
+			System.out.println("|     1 - Configure server address     |");
+			System.out.println("|     2 - View candidates              |");
+			System.out.println("|     3 - Vote for a candidate         |");
+			System.out.println("|     4 - Election result              |");
+			System.out.println("|     0 - Sair                         |");
+			System.out.println("========================================\n");
+			System.out.print("> ");
+			
+			// Select option
+			option = input.nextInt();
+			System.out.print("\n");
+			
+			// Execute option
+			switch (option) {
+				case 1:
+					break;
+					
+				case 2:
+					candidates();
+					break;
+					
+				case 3:
+					//vote();
+					break;
+					
+				case 4:
+					//result();
+					break;
+			}
+			
+			pause();
+		} while (option != 0);
+	}
+	
+	public static void pause() {
+		System.out.println("Press Enter to continue…");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
-	public static String[] candidate() {
+	public static void candidates() {
 		try {
-			return server.candidates();
+			for(String names : server.candidates())
+				System.out.println(names);
 		} catch (RemoteException e) {
 			System.err.println("[ERROR] Client on getting candidates: " + e.getMessage());
 			e.printStackTrace();
 		}
-		
-		return null;
 	}
 
 	public static boolean connect() {
