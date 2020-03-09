326e89a1571af61073d3a9c39d2163569cd3ecac
Bruno Marini <bruno.marini1@gmail.com>
2020-03-08 23:58:51 -0300

    [+] Client methods {hash, result}

--- src/client/Client.java
+++ src/client/Client.java
@@ -5,17 +5,39 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
+import server.Candidate;
 import server.remoteInterfaces.ElectionRemoteInterface;
 
 public class Client {
 	private static Registry registry;
 	private static ElectionRemoteInterface server;
 	
+	private static int hashClient = generateHash(0, 424242);
+	
 	public static void main(String[] args) {
 		if (connect())
 			menu();
+	}
+	
+	public static boolean connect() {
+		try {
+			registry = LocateRegistry.getRegistry("localhost");	
+			server = (ElectionRemoteInterface) registry.lookup("Election");
+			
+			// Success
+			System.out.println("[log] Servidor conectado");
+			return true;
+		} catch (RemoteException | NotBoundException e) {
+			System.err.println("[ERROR] Client on connect: " + e.getMessage());
+		}
+		
+		// Error
+		System.err.println("[ERROR] Não foi possível conectar no servidor :(");
+		return false;
 	}
 	
 	public static void menu() {
@@ -36,7 +58,6 @@ public class Client {
 			
 			// Select option
 			option = input.nextInt();
-			System.out.print("\n");
 			
 			// Execute option
 			switch (option) {
@@ -48,14 +69,16 @@ public class Client {
 					break;
 					
 				case 3:
-					//vote();
+					System.out.println("Candidate name: ");
+					vote(input.next(), hashClient);
 					break;
 					
 				case 4:
-					//result();
+					result();
 					break;
 			}
 			
+			System.out.println();
 			pause();
 		} while (option != 0);
 	}
@@ -69,6 +92,11 @@ public class Client {
 		}
 	}
 	
+	public static int generateHash(int min, int max) {
+	    Random r = new Random();
+	    return r.nextInt((max - min) + 1) + min;
+	}
+	
 	public static void candidates() {
 		try {
 			for(String names : server.candidates())
@@ -78,21 +106,28 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
-
-	public static boolean connect() {
+	
+	public static void vote(String candidate, int hash) {
 		try {
-			registry = LocateRegistry.getRegistry("localhost");	
-			server = (ElectionRemoteInterface) registry.lookup("Election");
-			
-			// Success
-			System.out.println("[log] Servidor conectado");
-			return true;
-		} catch (RemoteException | NotBoundException e) {
-			System.err.println("[ERROR] Client on connect: " + e.getMessage());
+			server.vote(candidate, hash);
+		} catch (RemoteException e) {
+			System.err.println("[ERROR] Client on voting: " + e.getMessage());
+			e.printStackTrace();
 		}
 		
-		// Error
-		System.err.println("[ERROR] Não foi possível conectar no servidor :(");
-		return false;
+		System.out.println("Successful vote for candidate: " + candidate);
+	}
+	
+	public static void result() {
+		try {
+			int place = 1;
+			for(Candidate candidate : server.result()) {
+				System.out.println(place + "º - " + candidate.name + " | " + candidate.votes + " votos");
+				place++;
+			}
+		} catch (RemoteException e) {
+			System.err.println("[ERROR] Client on election result: " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
